fa1cc3e9632a4c0549d800b6ff0c4662e4e78c9d
Anand Raj <anadraj73073@gmail.com>
2026-02-13 16:27:27 +0530

    add text-black to modal

--- src/app/components/ui/Modal.tsx
+++ src/app/components/ui/Modal.tsx
@@ -7,9 +7,9 @@ export function Modal({
 }) {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center">
-      <div className="bg-white p-6 rounded shadow w-96">
-        <h3 className="font-semibold mb-2">{title}</h3>
-        <p className="text-sm">{content}</p>
+      <div className="bg-white p-6 rounded shadow w-96 ">
+        <h3 className="font-semibold mb-2 text-black">{title}</h3>
+        <p className="text-sm text-black">{content}</p>
       </div>
     </div>
   )
